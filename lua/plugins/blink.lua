06bf07760b37fb21576f4f2b96799d8ba9ec987c
killinghorizons <jeremy.pro.lp@gmail.com>
2026-05-22 16:23:21 +0200

    anotha one and format bufferline for it

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -16,6 +16,7 @@ return {
             ["<C-\\>"] = { "hide", "fallback" },
             ["<C-n>"] = { "select_next", "show" },
             ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+            ["<S-Tab>"] = { "select_prev", "snippet_forward", "fallback" },
             ["<C-p>"] = { "select_prev" },
             ["<C-b>"] = { "scroll_documentation_up", "fallback" },
             ["<C-f>"] = { "scroll_documentation_down", "fallback" },
